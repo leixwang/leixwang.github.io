92e0dcf739e651fdd42aad590c9f592218bfd110
Richard Wang <wangleihd@gmail.com>
2023-07-20 11:34:20 +0800

    Fixed import file error.

---
+++ markdown/code/snippets/snippet-with-region.js
@@ -0,0 +1,12 @@
+export default { // Highlighted
+  data () {
+    return {
+			name: 'MyComponent',
+      msg: `Highlighted!
+      This line isn't highlighted,
+      but this and the next 2 are.`,
+      motd: 'VitePress is awesome',
+      lorem: 'ipsum'
+    }
+  }
+}
---
+++ markdown/code/snippets/snippet.js
@@ -0,0 +1,11 @@
+export default {
+  data () {
+    return {
+      msg: `Highlighted!
+      This line isn't highlighted,
+      but this and the next 2 are.`,
+      motd: 'VitePress is awesome',
+      lorem: 'ipsum'
+    }
+  }
+}
